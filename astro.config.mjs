afb3386f286102512bd1fb90f053d1ab25cc48fc
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-08-26 23:35:22 -0300

    feat: change vercel config to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gabrielforster.github.io",
   base: "portfolio",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercelStatic()
 });
